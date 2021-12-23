d4fc951f6dfc1d763c7c243011d2ba76fe0990d4
manuelamaza <manucaromaza@gmail.com>
2021-12-22 18:31:11 -0800

    subiendo mercado liebre

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, 'public');
+const port = 3030
+
+app.use(express.static(publicPath));
+
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.listen(process.env.PORT || 3030, () => {
+    console.log("El servidor esta corriendo en el puerto 3000")
+})
